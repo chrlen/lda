d478fd52b3e104b6a8b9101d5572bb9b9702c5ae
Christian Lengert <clengert@googlemail.com>
2018-12-01 00:30:04 +0100

    add dataset

--- sample.py
+++ sample.py
@@ -1,7 +1,9 @@
 from lda.dataset import DataSet
 from lda.inference import LDA
 
-dataset = DataSet(path='dataset/small.xml')
+path = 'dataset/small.xml'
+#path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
 
+dataset = DataSet(path=path)
 model = LDA()
 model.fit(dataset)
