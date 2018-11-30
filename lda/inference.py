74c196a99ffe619418e2d649e20154a9373f3d30
Christian Lengert <clengert@googlemail.com>
2018-12-01 00:28:45 +0100

    add iteration loop

--- lda/inference.py
+++ lda/inference.py
@@ -8,19 +8,51 @@ class LDA():
                  verbose=True
                  ):
         self.verbose = verbose
+        self.maxit = maxit
 
+        self.alpha = None
+        self.beta = None
+        self.iterations = 0
+        self.converged = False
+
+    def expectation(self):
+        """ For each document, find the optimizing values of the variational parameters """
+        print("Expect")
+
+    def maximization(self):
+        """ Maximize the resulting lower bound on the log likelihood """
+        print("Maximize")
+
+    def numOfDocuments():
+        return this.dataset.numOfDocuments()
+
+    def num
 
     def fit(self, dataset):
         if self.verbose:
             print("LDA => fitting to Dataset: " + str(dataset.matrix.shape))
 
         start = time.perf_counter()
+
+        while not self.converged:
+            self.expectation()
+            self.maximization()
+            self.iterations += 1
+            if self.verbose:
+                print("LDA.fit() => iteration: " + str(self.iterations))
+            if self.iterations > self.maxit:
+                self.converged = True
+                if self.verbose:
+                    print("LDA.fit() => Maximum number of iterations reached!")
+
         end = time.perf_counter()
 
         self.inferenceTime = end - start
 
         if self.verbose:
-            print("LDA => Fitting took: " + "{:10.4f}".format(self.inferenceTime) + "s")
+            print("LDA => Fitting took: "
+                  + "{:10.4f}".format(self.inferenceTime) + "s")
+
 
 if __name__ == '__main__':
     dataset = DataSet()
