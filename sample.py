f4529c5c439836558074f2dc432d74b59834feee
Christian Lengert <clengert@googlemail.com>
2019-01-17 10:31:34 +0100

    add dependencies

--- sample.py
+++ sample.py
@@ -1,6 +1,10 @@
 from lda.dataset import DataSet
 from lda.inference import LDA
 
+import scipy.stats as spst
+import numpy as np
+import numpy.random as npr
+
 path = 'dataset/small.xml'
 #path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
 
