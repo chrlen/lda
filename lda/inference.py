1e9887db7be4c5fcce8bf21ec2bede2ccb2d9d35
Christian Lengert <clengert@googlemail.com>
2019-01-18 13:02:44 +0100

    fix indentation, add comments with dimension of datastructures

--- lda/inference.py
+++ lda/inference.py
@@ -16,7 +16,12 @@ class LDA():
     def __init__(self,
                  maxit=1000,
                  verbose=True,
-                 readOutIterations=10
+                 readOutIterations=10,
+                 estimateHyperparameters=True,
+                 # Mixture proportions; length = num of topics
+                 alpha=None,
+                 # Mixture components ; length = num of terms
+                 beta=None
                  ):
         self.verbose = verbose
         self.maxit = maxit
@@ -28,6 +33,9 @@ class LDA():
         self.readOutIterations = readOutIterations
         self.lastReadOut = 0
 
+        if self.verbose:
+            print("LDA-Model => constructed")
+
     def numOf(self):
         return self.dataset
 
@@ -35,28 +43,33 @@ class LDA():
             nTopics=5):
         self.nTopics = nTopics
         self.dataset = dataset
+        if self.alpha == None:
+            self.alpha = np.repeat(50 / nTopics, nTopics)
+        if self.beta == None:
+            self.beta = np.repeat(0.01, nTopics)
 
-# -------------------------------- Initialize --------------------------------
+        if self.verbose:
+            print("LDA-Model => fitting to dataset")
         start = time.perf_counter()
 
-        # M: Number of documents
-        # K: Number of topics
-        # V: number of Terms
+       # M: Number of documents
+       # K: Number of topics
+       # V: number of Terms
 
-        # z_mn : W
+       # z_mn : W
         self.topicAssociations_z = hlp.randomMultimatrix(
             dataset.numOfDocuments(),
             dataset.dictionarySize(),
             nTopics)
 
-        # M x K
-        self.documentTopic_count_n_m = np.zeros(
+       # M x K
+        self.documentTopic_count_n_mk = np.zeros(
             (dataset.numOfDocuments(),
              nTopics)
         )
 
-        # K x v
-        self.topicTerm_count_n_k = np.zeros(
+  # K x v
+        self.topicTerm_count_n_kt = np.zeros(
             (nTopics,
              dataset.dictionarySize())
         )
@@ -64,12 +77,12 @@ class LDA():
         for document in range(dataset.numOfDocuments()):
             for term in range(dataset.dictionarySize()):
                 topic = self.topicAssociations_z[document, term]
-                self.documentTopic_count_n_m[document, topic] += 1
-                self.topicTerm_count_n_k[topic, term] += 1
+                self.documentTopic_count_n_mk[document, topic] += 1
+                self.topicTerm_count_n_kt[topic, term] += 1
 
-        # M
+    # M
         self.documentTopic_sum_n_m = np.sum(
-            self.documentTopic_count_n_m, axis=1)
+            self.documentTopic_count_n_mk, axis=1)
         assert(
             len(self.documentTopic_sum_n_m.shape) == 1
         )
@@ -78,7 +91,7 @@ class LDA():
         )
 
         # K
-        self.topicTerm_sum_n_k = np.sum(self.topicTerm_count_n_k, axis=1)
+        self.topicTerm_sum_n_k = np.sum(self.topicTerm_count_n_kt, axis=1)
         assert(
             len(self.topicTerm_sum_n_k.shape) == 1
         )
@@ -89,8 +102,8 @@ class LDA():
         end = time.perf_counter()
         self.initializazionTime = end - start
         if self.verbose:
-            print("LDA => Initialization took: " +
-                  "{:10.4f}".format(self.initializazionTime) + "s")
+            print("LDA => Initialization took: "
+                  + "{:10.4f}".format(self.initializazionTime) + "s")
 
 # -------------------------------- Burn-In phase --------------------------
 # -------------------------------- Sampling --------------------------------
@@ -100,9 +113,32 @@ class LDA():
         start = time.perf_counter()
 
         for iteration in tqdm(range(self.maxit), desc='EM: '):
-            # For the current assignment of k to a term t for word w_{m,n}
-            # multinomial sampling acc. to Eq. 78 (decrements from previous step)
-            # For new assignments of z_{m,n} to the term t for word w_{m,n}
+
+            for documentIndex in range(len(dataset.documents)):
+                document = dataset.documents[documentIndex]
+                for wordIndex in range(len(document)):
+                    word = document[wordIndex]
+                    wordId = dataset.dictionary.token2id[word]
+                    previousTopicIndex = self.topicAssociations_z[documentIndex, wordId]
+
+                    # For the current assignment of k to a term t for word w_{m,n}
+                    self.documentTopic_count_n_mk[documentIndex,
+                                                  previousTopicIndex] -= 1
+                    self.documentTopic_sum_n_m[documentIndex] -= 1
+                    self.topicTerm_count_n_kt[previousTopicIndex, wordId] -= 1
+                    self.topicTerm_sum_n_k[previousTopicIndex] -= 1
+                    # multinomial sampling acc. to Eq. 78 (decrements from previous step)
+
+                    newTopicIndex = 0
+
+                    self.topicAssociations_z[documentIndex,
+                                             wordId] = newTopicIndex
+                    # For new assignments of z_{m,n} to the term t for word w_{m,n}
+                    self.documentTopic_count_n_mk[documentIndex,
+                                                  newTopicIndex] += 1
+                    self.documentTopic_sum_n_m[documentIndex] += 1
+                    self.topicTerm_count_n_kt[newTopicIndex, wordId] += 1
+                    self.topicTerm_sum_n_k[newTopicIndex] += 1
 
             self.iterations += 1
             # if self.verbose:
@@ -121,8 +157,9 @@ class LDA():
         self.inferenceTime = end - start
 
         if self.verbose:
-            print("LDA => Fitting took: " +
-                  "{:10.4f}".format(self.inferenceTime) + "s")
+            print("LDA => Fitting took: {:10.4f}".format(
+                self.inferenceTime) + "s")
+            print("LDA => Convergence took: {:10.4f}".format(self.iterations))
 
 
 if __name__ == '__main__':
