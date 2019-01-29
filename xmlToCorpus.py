f80aeca73b79b2f0b7e9dd40cfba8204680cc3e6
Christian Lengert <clengert@googlemail.com>
2019-01-29 22:29:28 +0100

    rename

---
+++ xmlToCorpus.py
@@ -0,0 +1,24 @@
+from gensim.models.ldamulticore import LdaMulticore
+from lda.dataset import DataSet
+import multiprocessing as mp
+
+import scipy.stats as spst
+import numpy as np
+import numpy.random as npr
+
+import pickle
+
+from gensim.test.utils import datapath
+
+#path = 'dataset/small.xml'
+cores = mp.cpu_count() - 1
+path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
+savePath = "dataset/large/"
+
+dataset = DataSet(path=path)
+
+with open(savePath + 'corpus.pickle', 'wb') as handle:
+    pickle.dump(dataset.documents, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(savePath + 'dictionary.pickle', 'wb') as handle:
+    pickle.dump(dataset.dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
