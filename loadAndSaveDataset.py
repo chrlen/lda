e6a0faf465101cee82f5dfff1046ce26a502c0a6
Christian Lengert <clengert@googlemail.com>
2019-01-29 20:06:09 +0100

    pickle dataset

--- loadAndSaveDataset.py
+++ loadAndSaveDataset.py
@@ -6,11 +6,19 @@ import scipy.stats as spst
 import numpy as np
 import numpy.random as npr
 
+import pickle
+
 from gensim.test.utils import datapath
 
-#path = 'dataset/small.xml'
+path = 'dataset/small.xml'
 cores = mp.cpu_count() - 1
 path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
-savePath = "~/Desktop/dataset"
+#savePath = "/home/me/Desktop/simpleWiki/"
 
 dataset = DataSet(path=path)
+
+with open(savePath + 'corpus.pickle', 'wb') as handle:
+    pickle.dump(dataset.documents, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(savePath + 'dictionary.pickle', 'wb') as handle:
+    pickle.dump(dataset.dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
