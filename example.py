202352c534178d768c92fc59d7c633508e9b3473
Christian Lengert <clengert@googlemail.com>
2019-02-16 16:28:56 +0100

    add example

---
+++ example.py
@@ -0,0 +1,12 @@
+from lda.inference import LDA
+from lda.dataset import DataSet
+
+
+#path = 'dataset/small.xml'
+path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
+
+dataset = DataSet()
+dataset.loadFromDir('dataset/small/')
+model = LDA(maxit=3)
+model.fit(dataset)
+model.saveToDir('models/small/')
