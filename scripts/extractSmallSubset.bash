a8e8347db701164a550ed6cb1afe5d7df48b0f78
Christian Lengert <clengert@googlemail.com>
2019-02-16 20:44:38 +0100

    formatting

--- scripts/extractSmallSubset.bash
+++ scripts/extractSmallSubset.bash
@@ -3,14 +3,14 @@
 cd dataset
 
 #Choose how many pages to extract
-n=20
+n=100
 #Get line number of n-th occurence of the closing tag "</page>"
 lineNum=$(
   grep -n  "</page>" simplewiki-20181120-pages-meta-current.xml | \
     head -n${n} | \
     tail -n1 | \
     cut -d: -f1 \
-  )
+)
 
 #Copy large set upto lineNum into the file small.xml
 head simplewiki-20181120-pages-meta-current.xml -n${lineNum} > small.xml
