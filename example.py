cf7a609be2bc3af6a50361d3f471f69bcb4b897a
Christian Lengert <clengert@googlemail.com>
2019-02-16 20:44:01 +0100

    add parameters

--- example.py
+++ example.py
@@ -2,11 +2,10 @@ from lda.inference import LDA
 from lda.dataset import DataSet
 
 
-#path = 'dataset/small.xml'
+#xpath = 'dataset/small.xml'
 path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
-
 dataset = DataSet()
-dataset.loadFromDir('dataset/small/')
+dataset.loadFromDir(path)
 model = LDA(maxit=3)
-model.fit(dataset)
+model.fit(dataset, nTopics=30)
 model.saveToDir('models/small/')
