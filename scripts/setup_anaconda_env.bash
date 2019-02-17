f454ddedc0fe194504c56e20f5f6559d8fc487c8
Christian Lengert <clengert@googlemail.com>
2019-02-17 10:52:43 +0100

    clean file

--- scripts/setup_anaconda_env.bash
+++ scripts/setup_anaconda_env.bash
@@ -1,7 +1,2 @@
 #! /bin/bash
-
 conda create -y --name lda pandas scipy tqdm gensim python=3.7
-source activate lda
-conda install -y -c anaconda gensim pattern
-#conda install -y pandas scipy tqdm
-#conda install -y -c conda-forge pyldavis
