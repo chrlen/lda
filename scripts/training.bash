1c825551bb70b629924feb34441aaf732d19e6bf
Christian Lengert <clengert@googlemail.com>
2019-02-17 10:53:03 +0100

    remove training.bash

--- scripts/training.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-source activate lda
-
-python preprocess.py 'dataset/small.xml' "dataset/small/"
-python preprocess.py 'dataset/simplewiki-20181120-pages-meta-current.xml' "dataset/large/"
