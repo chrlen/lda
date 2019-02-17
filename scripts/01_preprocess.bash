5b2a959cd587a24e72bdda43a8c6aa541eec576c
Christian Lengert <clengert@googlemail.com>
2019-02-17 09:12:37 +0100

    restructure

--- scripts/01_preprocess.bash
+++ scripts/01_preprocess.bash
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-source activate lda
 sh scripts/extractSmallSubset.bash
-rm -rf dataset/small/*
-rm -rf dataset/full/*
-ipython preprocess.py dataset/small.xml dataset/small/ &
-ipython preprocess.py dataset/simplewiki-20181120-pages-meta-current.xml dataset/large/ &
+python preprocess.py dataset/small.xml dataset/small/ &
+python preprocess.py dataset/simplewiki-20181120-pages-meta-current.xml dataset/full/ &
