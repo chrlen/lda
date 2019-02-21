46bd8eeff99dcf0a216aadc949fce9d974aafbf0
Christian Lengert <clengert@googlemail.com>
2019-02-21 01:04:34 +0100

    fix paths

--- scripts/03_trainingParallel.bash
+++ scripts/03_trainingParallel.bash
@@ -6,8 +6,8 @@ iterations=3
 topics=20
 
 modeldir=$(date | sed -e "s/ /_/g")_${iterations}_${topics}
-mkdir /tmp/models/${modeldir}
-mkdir /tmp/models/${modeldir}/small
-mkdir /tmp/models/${modeldir}/full
-python trainingParallel.py ${iterations} ${topics} dataset/small /tmp/models/${modeldir}/small
+mkdir models/${modeldir}
+mkdir models/${modeldir}/small
+mkdir models/${modeldir}/full
+python trainingParallel.py ${iterations} ${topics} dataset/small models/${modeldir}/small
 #python training.py ${iterations} ${topics} dataset/full models/${modeldir}/full
