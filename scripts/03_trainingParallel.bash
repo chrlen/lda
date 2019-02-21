463fdbbc9c35845a15596ee8f14508908e110c50
Christian Lengert <clengert@googlemail.com>
2019-02-21 09:30:15 +0100

    train on large dataset

--- scripts/03_trainingParallel.bash
+++ scripts/03_trainingParallel.bash
@@ -9,5 +9,5 @@ modeldir=$(date | sed -e "s/ /_/g")_${iterations}_${topics}
 mkdir models/${modeldir}
 mkdir models/${modeldir}/small
 mkdir models/${modeldir}/full
-python trainingParallel.py ${iterations} ${topics} dataset/small models/${modeldir}/small
-#python training.py ${iterations} ${topics} dataset/full models/${modeldir}/full
+#python trainingParallel.py ${iterations} ${topics} dataset/small models/${modeldir}/small
+python training.py ${iterations} ${topics} dataset/full models/${modeldir}/full
