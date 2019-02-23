e446785c7aa4b185c2189d29cfc66055a696f822
Christian Lengert <clengert@googlemail.com>
2019-02-23 19:44:26 +0100

    add example for saving and loading datasets

--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 from lda.inference import LDA
 from lda.dataset import DataSet
 
-
-#xpath = 'dataset/small.xml'
 path = 'dataset/simplewiki-20181120-pages-meta-current.xml'
 dataset = DataSet()
-dataset.loadFromDir(path)
-model = LDA(maxit=3)
-model.fit(dataset, nTopics=30)
+dataset.load(path)
+dataset.saveToDir('/tmp/dataset/')
+
+loadedDataset = DataSet()
+loadedDataset.loadFromDir('/tmp/dataset/')
+
+model = LDA(maxit=500)
+model.fitParallel(loadedDataset, nTopics=30)
 model.saveToDir('models/small/')
